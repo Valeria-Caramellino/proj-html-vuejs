21fc80b85eb3649bf6fc26618f9897b931212a5f
valeria caramellino <valeriacaramellino@gmail.com>
2023-05-30 16:53:04 +0200

    fix header

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -13,18 +13,21 @@ export default {
 
 <template>
   <main>
-    <h1>Main:</h1>
-    <ul>
-      <li>piccole icone</li>
-      <li>elemento 2 card uguale o simile ad altro elemento</li>
-      <li>categorie speciali</li>
-      <li>oup products carosello tutti oggetti</li>
-      <li>elemento 2 card uguale o simile al secondo punto</li>
-      <li>deal of the day carosello con oggetti scontati</li>
-      <li>fascia vh50 carosello con puntini</li>
-      <li>blog new game carosello</li>
-      <li>brend</li>
-    </ul>
+
+  <!--
+      <h1>Main:</h1>
+      <ul>
+        <li>piccole icone</li>
+        <li>elemento 2 card uguale o simile ad altro elemento</li>
+        <li>categorie speciali</li>
+        <li>oup products carosello tutti oggetti</li>
+        <li>elemento 2 card uguale o simile al secondo punto</li>
+        <li>deal of the day carosello con oggetti scontati</li>
+        <li>fascia vh50 carosello con puntini</li>
+        <li>blog new game carosello</li>
+        <li>brend</li>
+      </ul>
+      -->
   </main>
 </template>
 
